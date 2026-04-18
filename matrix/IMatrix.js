48a0c1936c8db3419f3063fdda52d9dc805790b6
FireDude123 <153016828+f1redude123@users.noreply.github.com>
2026-04-18 18:12:31 -0400

    Update IMatrix.js

--- matrix/IMatrix.js
+++ matrix/IMatrix.js
@@ -1,5 +1,34 @@
 export default class IMatrix {
   sizeX;
   sizeY;
-  data = [];
+  data = new Array(this.sizeX*this.sizeY);
+  
+  mul(mat) {
+    if (this.constructor == IMatrix || mat.constructor == IMatrix) {
+      throw new Error("Cannot perform matrix operations on object of type IMatrix");
+    }
+    
+    var newMat = new this.constructor();
+    newMat.sizeX = Math.min(this.sizeX, mat.sizeX);
+    newMat.sizeY = Math.min(this.sizeY, mat.sizeY);
+    for (var i = 0; i < this.sizeY; i++) {
+      for (var n = 0; n < mat.sizeX; n++) {
+        var res = 0;
+
+        for (var r = 0; r < this.sizeX; r++) {
+          res += this.getAt(r, i) * mat.getAt(n, r);
+        }
+
+        newMat.setAt(n, i, res);
+      }
+    }
+  }
+
+  getAt(x, y) {
+    return this.data[x + y * this.sizeX];
+  }
+  
+  setAt(x, y, n) {
+    this.data[x + y * this.sizeX] = n;
+  }
 }
