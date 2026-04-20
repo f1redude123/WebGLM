c2686f069ecbe1d09bf56ead816ae242fc986fed
FireDude123 <153016828+f1redude123@users.noreply.github.com>
2026-04-20 12:06:14 -0400

    Create main.js

---
+++ vector/main.js
@@ -0,0 +1,13 @@
+import IVector from "https://f1redude123.github.io/WebGLM/vector/IVector.js";
+
+export class Vector4 extends IVector {
+  size = 4;
+}
+
+export class Vector3 extends IVector {
+  size = 3;
+}
+
+export class Vector2 extends IVector {
+  size = 2;
+}
